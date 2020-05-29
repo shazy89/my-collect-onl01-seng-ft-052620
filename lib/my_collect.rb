cbb36748ae129e220eb75493f95d8103a49ef565
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-29 14:59:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,12 @@
 def my_collect(array)
+
   modified_array = []
   index = 0
-  
-  while index < array.length
-  modified_array << (array[index])
-  index +=1
-end 
-modified_array
-end
 
+  while index < array.length 
+    modified_array << yield(array[index])
+    index += 1 
+  end
+
+  modified_array
+end
